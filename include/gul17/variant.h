e999e67e95632fefd7b86cb67d8f6a7aa4dafc1a
Lars Froehlich <lars.froehlich@desy.de>
2025-05-23 14:00:47 +0200

    Re-import OverloadSet and make_overload_set() from GUL14
    [why]
    Both the OverloadSet class template and the make_overload_set() function
    template were included in GUL14, but removed along with the variant.h
    header in the transition to GUL17. However, at least the class template
    is still useful in conjunction with std::visit().
    
    [how]
    Import both the class and the function template. Rewrite OverloadSet in
    C++17 style with a deduction guide so it can be constructed directly
    from an arbitrary list of lambdas. Deprecate the make_overload_set()
    function because it no longer offers any benefit over instantiating an
    OverloadSet directly. Import the original tests for make_overload_set().
    
    Signed-off-by: Lars Froehlich <lars.froehlich@desy.de>

---
+++ include/gul17/variant.h
@@ -0,0 +1,84 @@
+/**
+ * \file    variant.h
+ * \brief   Implementation of the OverloadSet class template and the make_overload_set()
+ *          function template.
+ * \authors \ref contributors
+ * \date    Created on 15 June 2023
+ *
+ * \copyright Copyright 2023-2025 Deutsches Elektronen-Synchrotron (DESY), Hamburg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef GUL17_VARIANT_H_
+#define GUL17_VARIANT_H_
+
+namespace gul17 {
+
+/**
+ * \addtogroup variant_h gul17/variant.h
+ * \brief Implementation of the OverloadSet class template and the make_overload_set()
+ *        function template.
+ * @{
+ */
+
+/**
+ * A function object that works like an overload set of functions.
+ *
+ * \code{.cpp}
+ * std::variant<int, double> v = 1.0;
+ * auto str = std::visit(
+ *     gul17::OverloadSet{
+ *         [](char) { return "char"; },
+ *         [](int) { return "int"; }
+ *     }, v);
+ * \endcode
+ *
+ * This variadic class template inherits from an arbitrary number of function objects
+ * (typically lambdas) and imports their definitions of `operator()` into an overload set
+ * for its own `operator()`. This is primarily helpful for use with std::visit().
+ *
+ * \since GUL14 version 2.9.2; dropped in GUL17 25.3.0; reintroduced in GUL17 version
+ *        UNRELEASED
+ */
+template<typename... Fcts>
+struct OverloadSet : Fcts ...
+{
+    using Fcts::operator()...;
+};
+
+template<typename... Fcts>
+OverloadSet(Fcts...) -> OverloadSet<Fcts...>;
+
+/**
+ * Create an OverloadSet from an arbitrary number of function objects.
+ *
+ * This function creates an OverloadSet from the function objects given to it. This used
+ * to be helpful for visiting a variant prior to C++17, but offers no benefit over
+ * constructing an OverloadSet directly.
+ *
+ * \since GUL14 version 2.9.2; dropped in GUL17 25.3.0; reintroduced in GUL17 version
+ *        UNRELEASED
+ * \deprecated since GUL17 version UNRELEASED
+ */
+template <typename... Fct>
+[[deprecated("Construct an OverloadSet directly instead")]]
+auto make_overload_set(Fct... f)
+{
+    return OverloadSet{ f... };
+}
+
+} // namespace gul17
+
+#endif
