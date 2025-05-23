0dafb7c1b9539a545585b486b013999e1f851ade
Lars Froehlich <lars.froehlich@desy.de>
2025-05-23 14:00:47 +0200

    Re-import OverloadSet and make_overload_set() from GUL14
    [why]
    Both the OverloadSet class template and the make_overload_set() function
    template were included in GUL14, but removed along with the variant.h
    header in the transition to GUL17. However, at least the class template
    is still useful in conjunction with std::visit().
    
    [how]
    Import both the class and the function template. Rewrite OverloadSet in
    C++17 style with a deduction guide so it can be constructed directly
    from an arbitrary list of lambdas. Deprecate the make_overload_set()
    function because it no longer offers any benefit over instantiating an
    OverloadSet directly. Import the original tests for make_overload_set().
    
    Signed-off-by: Lars Froehlich <lars.froehlich@desy.de>

---
+++ tests/test_variant.cc
@@ -0,0 +1,51 @@
+/**
+ * \file   test_variant.cc
+ * \author \ref contributors
+ * \date   Created on May 31, 2023
+ * \brief  Unit tests for the variant class template.
+ *
+ * \copyright Copyright 2023-2025 Deutsches Elektronen-Synchrotron (DESY), Hamburg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <variant>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "gul17/variant.h"
+
+TEST_CASE("make_overload_set()", "[variant]")
+{
+    auto v = std::variant<float, double, long, std::string>{ };
+
+    auto get_type_name = gul17::make_overload_set(
+        [](auto&&) -> std::string { return "SOMETHING ELSE"; },
+        [](float) -> std::string { return "float"; },
+        [](double) -> std::string { return "double"; },
+        [](long) -> std::string { return "long"; });
+
+    v = 1.5f;
+    REQUIRE(std::visit(get_type_name, v) == "float");
+
+    v = 2.4;
+    REQUIRE(std::visit(get_type_name, v) == "double");
+
+    v = 42L;
+    REQUIRE(std::visit(get_type_name, v) == "long");
+
+    v = "Test";
+    REQUIRE(std::visit(get_type_name, v) == "SOMETHING ELSE");
+}
