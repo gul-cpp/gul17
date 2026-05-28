6fbe88f93dffa5993ace4050e6f632a0db7546aa
Lars Froehlich <lars.froehlich@desy.de>
2026-05-28 13:46:26 +0200

    Change license in file headers to MIT
    [why]
    We have put our source code under the MIT license, so the
    file headers must reflect that.

--- include/gul17/gul.h
+++ include/gul17/gul.h
@@ -1,23 +1,26 @@
 /**
- * \file    gul.h
- * \authors \ref contributors
- * \date    Created on August 24, 2018
- * \brief   Main include file for the General Utility Library.
+ * \file  gul.h
+ * \date  Created on August 24, 2018
+ * \brief Main include file for the General Utility Library.
  *
  * \copyright Copyright 2018-2025 Deutsches Elektronen-Synchrotron (DESY), Hamburg
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 2.1 of the license, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef GUL17_GUL_H_
@@ -39,7 +42,6 @@
 #include "gul17/bit_manip.h"
 #include "gul17/case_ascii.h"
 #include "gul17/cat.h"
-// #include "gul17/catch.h" not included because it is only useful for unit tests
 // #include "gul17/date.h" not included by default to reduce compile times
 #include "gul17/escape.h"
 #include "gul17/expected.h"
