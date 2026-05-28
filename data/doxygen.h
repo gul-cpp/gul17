4355ae29de61623d54b386cfd94a1b31cf9c9e56
Lars Froehlich <lars.froehlich@desy.de>
2026-05-28 11:09:33 +0200

    Change main license to Boost Software License v1.0
    [why]
    Read https://www.boost.org/doc/user-guide/bsl.html for a rationale of
    why the BSL is a good choice especially for foundational C++ libraries.
    In a nutshell, most other licenses require a copyright notice to appear
    in the final product even when the library is used in binary form, which
    the BSL does not require. This makes the library easier to use in other
    software.
    
    [how]
    Replace the old license.txt with the LGPL-2.1 text by a LICENSE.md with
    the BSL 1.0 text. Move our copyright and license information from the
    documentation website into README.md where it is much more accessible.
    The website just retains a link to the BSL and to the README. Adjust
    some details of the main Doxygen page accordingly (e.g. move the list
    of contributors into README.md).

--- data/doxygen.h
+++ data/doxygen.h
@@ -99,16 +99,18 @@ namespace gul17 {
  * requests, but they should fit into the scope of the library – it may be better to
  * discuss new features in a GitHub issue before diving into the implementation.
  *
- * \section behind_the_scenes Behind the Scenes
+ * \section changelog Changelog
  *
- * \version
- * \ref release_notes
+ * Please refer to the \ref release_notes "Release Notes" page for information on changes
+ * between different versions of the library.
  *
- * \authors
- * \ref contributors
+ * \section copyright_license_contributors Copyright, License, and Contributors
  *
- * \copyright
- * \ref copyright_notice
+ * Most of the library is distributed under the
+ * \ref license_boost_1_0 "Boost Software License, Version 1.0", but exceptions apply for
+ * individual files. Please refer to the
+ * [README file](https://github.com/gul-cpp/gul17/blob/main/README.md) in the library
+ * repository for more information on copyright, license, and a list of contributors.
  */
 
 /**
@@ -350,60 +352,6 @@ namespace gul17 {
  *
  * - *Released with DOOCS 19.5.0*
  * - First release version of GUL
- */
-
-/**
- * \page contributors_and_copyright Contributors & Copyright
- *
- * \section contributors General Utility Library Contributors
- *
- * The following individuals contributed to the General Utility Library:
- *
- * > Lars Fröhlich, Sören Grunewald, Olaf Hensler, Fini Jastrow, Josef Wilgen
- *
- * Additional code distributed with this library was written by authors listed under
- * \ref additional_copyright_notices below.
- *
- * \section copyright_notice Copyright Notice
- *
- * Copyright 2018-2025 Deutsches Elektronen-Synchrotron (DESY), Hamburg
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 2.1 of the license, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * \section additional_copyright_notices Additional Copyright Notices
- *
- * This library contains third-party code that is distributed under its own copyright
- * notice:
- *
- * <dl>
- * <dt>date.h</dt>
- * <dd>Copyright 2015, 2016, 2017 Howard Hinnant, 2016 Adrian Colomitchi, 2017 Florian
- *     Dang, 2017 Paul Thompson, 2018, 2019 Tomasz Kamiński, 2019 Jiangang Zhuang.
- *     Distributed under the MIT License (see \ref license_mit and \ref date.h for
- *     details).</dd>
- *
- * <dt>\ref expected.h</dt>
- * <dd>Written in 2017 by Sy Brand. To the extent possible under law, the author(s) have
- *     dedicated all copyright and related and neighboring rights to this software to the
- *     public domain worldwide. This software is distributed without any warranty.
- *     \ref license_cc0_public_domain and \ref expected.h for details).</dd>
- *
- * <dt>\ref span.h</dt>
- * <dd>Copyright 2018 Tristan Brindle.
- *     Distributed under the Boost Software License, Version 1.0 (see
- *     \ref license_boost_1_0 and \ref span.h for details).</dd>
- * </dl>
  */
 
 /**
@@ -816,150 +764,6 @@ namespace gul17 {
  * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- */
-
-/**
- * \page license_cc0_public_domain Creative Commons Legal Code – CC0 1.0 Universal
- *
- *     CREATIVE COMMONS CORPORATION IS NOT A LAW FIRM AND DOES NOT PROVIDE
- *     LEGAL SERVICES. DISTRIBUTION OF THIS DOCUMENT DOES NOT CREATE AN
- *     ATTORNEY-CLIENT RELATIONSHIP. CREATIVE COMMONS PROVIDES THIS
- *     INFORMATION ON AN "AS-IS" BASIS. CREATIVE COMMONS MAKES NO WARRANTIES
- *     REGARDING THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS
- *     PROVIDED HEREUNDER, AND DISCLAIMS LIABILITY FOR DAMAGES RESULTING FROM
- *     THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS PROVIDED
- *     HEREUNDER.
- *
- * Statement of Purpose
- *
- * The laws of most jurisdictions throughout the world automatically confer
- * exclusive Copyright and Related Rights (defined below) upon the creator
- * and subsequent owner(s) (each and all, an "owner") of an original work of
- * authorship and/or a database (each, a "Work").
- *
- * Certain owners wish to permanently relinquish those rights to a Work for
- * the purpose of contributing to a commons of creative, cultural and
- * scientific works ("Commons") that the public can reliably and without fear
- * of later claims of infringement build upon, modify, incorporate in other
- * works, reuse and redistribute as freely as possible in any form whatsoever
- * and for any purposes, including without limitation commercial purposes.
- * These owners may contribute to the Commons to promote the ideal of a free
- * culture and the further production of creative, cultural and scientific
- * works, or to gain reputation or greater distribution for their Work in
- * part through the use and efforts of others.
- *
- * For these and/or other purposes and motivations, and without any
- * expectation of additional consideration or compensation, the person
- * associating CC0 with a Work (the "Affirmer"), to the extent that he or she
- * is an owner of Copyright and Related Rights in the Work, voluntarily
- * elects to apply CC0 to the Work and publicly distribute the Work under its
- * terms, with knowledge of his or her Copyright and Related Rights in the
- * Work and the meaning and intended legal effect of CC0 on those rights.
- *
- * 1. Copyright and Related Rights. A Work made available under CC0 may be
- * protected by copyright and related or neighboring rights ("Copyright and
- * Related Rights"). Copyright and Related Rights include, but are not
- * limited to, the following:
- *
- *   i. the right to reproduce, adapt, distribute, perform, display,
- *      communicate, and translate a Work;
- *  ii. moral rights retained by the original author(s) and/or performer(s);
- * iii. publicity and privacy rights pertaining to a person's image or
- *      likeness depicted in a Work;
- *  iv. rights protecting against unfair competition in regards to a Work,
- *      subject to the limitations in paragraph 4(a), below;
- *   v. rights protecting the extraction, dissemination, use and reuse of data
- *      in a Work;
- *  vi. database rights (such as those arising under Directive 96/9/EC of the
- *      European Parliament and of the Council of 11 March 1996 on the legal
- *      protection of databases, and under any national implementation
- *      thereof, including any amended or successor version of such
- *      directive); and
- * vii. other similar, equivalent or corresponding rights throughout the
- *      world based on applicable law or treaty, and any national
- *      implementations thereof.
- *
- * 2. Waiver. To the greatest extent permitted by, but not in contravention
- * of, applicable law, Affirmer hereby overtly, fully, permanently,
- * irrevocably and unconditionally waives, abandons, and surrenders all of
- * Affirmer's Copyright and Related Rights and associated claims and causes
- * of action, whether now known or unknown (including existing as well as
- * future claims and causes of action), in the Work (i) in all territories
- * worldwide, (ii) for the maximum duration provided by applicable law or
- * treaty (including future time extensions), (iii) in any current or future
- * medium and for any number of copies, and (iv) for any purpose whatsoever,
- * including without limitation commercial, advertising or promotional
- * purposes (the "Waiver"). Affirmer makes the Waiver for the benefit of each
- * member of the public at large and to the detriment of Affirmer's heirs and
- * successors, fully intending that such Waiver shall not be subject to
- * revocation, rescission, cancellation, termination, or any other legal or
- * equitable action to disrupt the quiet enjoyment of the Work by the public
- * as contemplated by Affirmer's express Statement of Purpose.
- *
- * 3. Public License Fallback. Should any part of the Waiver for any reason
- * be judged legally invalid or ineffective under applicable law, then the
- * Waiver shall be preserved to the maximum extent permitted taking into
- * account Affirmer's express Statement of Purpose. In addition, to the
- * extent the Waiver is so judged Affirmer hereby grants to each affected
- * person a royalty-free, non transferable, non sublicensable, non exclusive,
- * irrevocable and unconditional license to exercise Affirmer's Copyright and
- * Related Rights in the Work (i) in all territories worldwide, (ii) for the
- * maximum duration provided by applicable law or treaty (including future
- * time extensions), (iii) in any current or future medium and for any number
- * of copies, and (iv) for any purpose whatsoever, including without
- * limitation commercial, advertising or promotional purposes (the
- * "License"). The License shall be deemed effective as of the date CC0 was
- * applied by Affirmer to the Work. Should any part of the License for any
- * reason be judged legally invalid or ineffective under applicable law, such
- * partial invalidity or ineffectiveness shall not invalidate the remainder
- * of the License, and in such case Affirmer hereby affirms that he or she
- * will not (i) exercise any of his or her remaining Copyright and Related
- * Rights in the Work or (ii) assert any associated claims and causes of
- * action with respect to the Work, in either case contrary to Affirmer's
- * express Statement of Purpose.
- *
- * 4. Limitations and Disclaimers.
- *
- *  a. No trademark or patent rights held by Affirmer are waived, abandoned,
- *     surrendered, licensed or otherwise affected by this document.
- *  b. Affirmer offers the Work as-is and makes no representations or
- *     warranties of any kind concerning the Work, express, implied,
- *     statutory or otherwise, including without limitation warranties of
- *     title, merchantability, fitness for a particular purpose, non
- *     infringement, or the absence of latent or other defects, accuracy, or
- *     the present or absence of errors, whether or not discoverable, all to
- *     the greatest extent permissible under applicable law.
- *  c. Affirmer disclaims responsibility for clearing rights of other persons
- *     that may apply to the Work or any use thereof, including without
- *     limitation any person's Copyright and Related Rights in the Work.
- *     Further, Affirmer disclaims responsibility for obtaining any necessary
- *     consents, permissions or other rights required for any use of the
- *     Work.
- *  d. Affirmer understands and acknowledges that Creative Commons is not a
- *     party to this document and has no duty or obligation with respect to
- *     this CC0 or use of the Work.
- */
-
-/**
- * \page license_mit MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
  */
 
 /**
